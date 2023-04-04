e35d26445f27450b59b032f89b831af7e0d68ee8
Thomas Dorval <thomas.dorval.school@gmail.com>
2023-04-04 10:53:34 -0400

    The whitespace issues were bothering me
    Signed-off-by: Thomas Dorval <thomas.dorval.school@gmail.com>

--- lab4/Part2/rrt_robot.py
+++ lab4/Part2/rrt_robot.py
@@ -36,12 +36,12 @@ def step_from_to(node0, node1, limit=30):
 def node_generator(cmap):
     rand_node = None
 
-    while True: 
+    while True:
       rand_node = Node([random.uniform(0, cmap.width), random.uniform(0, cmap.height)])
 
       if (cmap.is_inbound(rand_node) and cmap.is_inside_obstacles(rand_node) == False):
           break
-    
+
     return rand_node
 
 
@@ -49,10 +49,10 @@ def RRT(cmap, start):
     cmap.add_node(start)
     global stopevent
     map_width, map_height = cmap.get_size()
-    
+
     while (cmap.get_num_nodes() < MAX_NODES):
         ########################################################################
-        
+
         rand_node = cmap.get_random_valid_node()
         nearest_node = None
         min_dist = float("inf")
@@ -67,7 +67,7 @@ def RRT(cmap, start):
             cmap.add_path(nearest_node, new_node)
         ########################################################################
         sleep(0.01)
-        
+
         if cmap.is_solved():
             for goal in cmap._goals:
                cur_path.append(goal)
@@ -75,7 +75,7 @@ def RRT(cmap, start):
                while cur_path[i].parent is not None:
                   cur_path.append(cur_path[i].parent)
                   i = i+1
-            
+
             break
 
     if cmap.is_solution_valid():
@@ -98,18 +98,18 @@ async def CozmoPlanning(robot: cozmo.robot.Robot):
     # has been closed by checking stopevent.is_set()
     global cmap, stopevent
     goal_found = 0
-    
+
     cube_obj1 = robot.world.get_light_cube(cozmo.objects.LightCube1Id)
 
     cube_obj2 = robot.world.get_light_cube(cozmo.objects.LightCube2Id)
-    
+
     cube_obj3 = robot.world.get_light_cube(cozmo.objects.LightCube3Id)
-    
+
     if (cube_obj1 is not None) and (cube_obj2 is not None) and (cube_obj3 is not None):
         print("All cubes defined successfully!")
     else:
         print("One or more object definitions failed!")
-       
+
     try:
         while True:
             event = await robot.world.wait_for(cozmo.camera.EvtNewRawCameraImage, timeout=30)   #get camera image
@@ -119,7 +119,7 @@ async def CozmoPlanning(robot: cozmo.robot.Robot):
                     robot.camera.enable_auto_exposure = True
                 else:
                     robot.camera.set_manual_exposure(exposure,fixed_gain)
-                    
+
             target_cube = await robot.world.wait_for_observed_light_cube(timeout=30)
             if(target_cube is not None):
                cube_matrix = np.matrix([[target_cube.pose.position.x],[target_cube.pose.position.y],[1]])
@@ -140,7 +140,7 @@ async def CozmoPlanning(robot: cozmo.robot.Robot):
                    await start_rrt()
                    while rrt_wait:
                      sleep(1)
-                   
+
                    path_coord_list = []
                    if cmap.is_solved():
                      print("solved")
@@ -154,7 +154,7 @@ async def CozmoPlanning(robot: cozmo.robot.Robot):
                         new_x = new_coord[0]
                         new_y = new_coord[1]
                         print("robot: ", new_x, new_y)
-                        
+
                         theta = np.arctan2(new_y-robot.pose.position.y, new_x-robot.pose.position.x)
                         distance = np.sqrt((new_x - robot.pose.position.x) ** 2 + (new_y - robot.pose.position.y) ** 2)
                         print("distance: ", distance)
@@ -163,8 +163,7 @@ async def CozmoPlanning(robot: cozmo.robot.Robot):
                            cozmo.util.radians(theta), in_parallel=True).wait_for_completed()
                         await robot.drive_straight(cozmo.util.distance_mm(distance),
                             cozmo.util.speed_mmps(300), in_parallel=True).wait_for_completed()
-                   
-                   
+
                if(target_cube.object_id == 2):
                   cmap.add_obstacle([Node([target_cube.pose.position.x-44, target_cube.pose.position.y]),
                      Node([target_cube.pose.position.x, target_cube.pose.position.y-44]),
@@ -179,11 +178,11 @@ async def CozmoPlanning(robot: cozmo.robot.Robot):
                            Node([target_cube.pose.position.x, target_cube.pose.position.y+44])])
                   cube3_x = robot_coord[0,0]
                   cube3_y = robot_coord[1,0]
-    
+
     ########################################################################
     # TODO: please enter your code below.
     # Description of function provided in instructions
-    
+
     except KeyboardInterrupt:
         print("")
         print("Exit requested by user")
@@ -195,7 +194,7 @@ async def start_rrt():
    RRT_thread = RRTThread()
    RRT_thread.start()
    stopevent.set()
-      
+
 ################################################################################
 #                     DO NOT MODIFY CODE BELOW                                 #
 ################################################################################
